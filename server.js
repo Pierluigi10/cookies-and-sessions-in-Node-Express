2c13b86b5b7a7f360ee973c5c15e23a4f55eeea4
pierluigi <pierluigibaiano@gmail.com>
2021-12-01 09:47:57 +0100

    imports session and cookieParser and add middleware

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 import express from "express";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = 5002;
 
-app.get('/', (req, res) => {
-    res.send("this is a test!")
-})
+app.use(cookieParser());
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "secret",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("this is a test!");
+});
 
 app.listen(port, (req, res) => {
   console.log(`API listening on port http://localhost:${port}`);
