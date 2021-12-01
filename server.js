1e05fd07f6558ca0491b1893652ab33452f138b0
pierluigi <pierluigibaiano@gmail.com>
2021-12-01 10:17:23 +0100

    add session management endpoints

--- server.js
+++ server.js
@@ -15,22 +15,46 @@ app.use(
 );
 
 const users = [
-    {
-        username: "ja",
-        firstName: "James",
-        lastName: "Anderson",
-        email: "ja@mail.com"
-    },
-    {
-        username: "ac",
-        firstName: "Ashley",
-        lastName: "Cartwright",
-        email: "ac@mail.com"
-    }
+  {
+    username: "ja",
+    firstName: "James",
+    lastName: "Anderson",
+    email: "ja@mail.com",
+  },
+  {
+    username: "ac",
+    firstName: "Ashley",
+    lastName: "Cartwright",
+    email: "ac@mail.com",
+  },
 ];
 
-app.get("/", (req, res) => {
-  res.send("this is a test!");
+// app.get("/", (req, res) => {
+//   res.send("this is a test!");
+// });
+
+app.get("/login/:username", (req, res) => {
+  const user = users.find((user) => user.username === req.params.username);
+  if (user) {
+    req.session.user = user;
+    req.session.save();
+    res.send(`User logged in: ${JSON.stringify(user)}`);
+  } else {
+    res.status(500).send("bad login");
+  }
+});
+
+app.get("/user", (req, res) => {
+    if (req.session.user) {
+        res.send(req.session.user);
+    } else {
+        res.send('no user logged in');
+    }
+});
+
+app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.send("User logged out");
 });
 
 app.listen(port, (req, res) => {
