527d7de70466aaa5cc5cfbbf2ed9051c567f627d
pierluigi <pierluigibaiano@gmail.com>
2021-12-01 09:43:12 +0100

    test the api  with get

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 5002;
 
+app.get('/', (req, res) => {
+    res.send("this is a test!")
+})
+
 app.listen(port, (req, res) => {
   console.log(`API listening on port http://localhost:${port}`);
 });
